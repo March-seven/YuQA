029a3e6e454a13ebfc10e1f5e97757832e87d079
ycy <1600535825@qq.com>
2024-11-18 22:23:30 +0800

    YuQA 前端框架搭建完成

---
+++ yuqa-frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <template v-if="route.path.startsWith('/user')">
+      <UserLayout />
+    </template>
+    <template v-else>
+      <BasicLayout />
+    </template>
+  </div>
+</template>
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import UserLayout from "@/layouts/UserLayout.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<style scoped></style>
---
+++ yuqa-frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,76 @@
+<template>
+  <a-row class="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="titleBar">
+            <img class="logo" src="../assets/logo.png" alt="logo" />
+            <div class="title">YuQA</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        <a-button type="primary" href="/user/login">登入</a-button>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+//当前选中的菜单页面
+const selectedKeys = ref(["/"]);
+//路由跳转时，自动更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+//展示在菜单栏的路由数组
+const visibleRoutes = routes.filter((item) => {
+  if (item.meta?.hideInMenu) {
+    return false;
+  }
+  return true;
+});
+// 点击菜单跳转到对应页面
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+<style scoped>
+#globalHeader {
+}
+
+.titleBar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
